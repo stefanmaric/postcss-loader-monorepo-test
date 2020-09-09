018456f44f6980248a2103d29c0f6d2ac1d03071
Stefan Maric <me@stefanmaric.com>
2020-09-09 16:57:18 +0200

    Simplify config and only use contrib packages

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,18 +1,16 @@
 const path = require('path')
 
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const safePostCssParser = require('postcss-safe-parser')
 
 const { NODE_ENV } = process.env
 
 const isProduction = NODE_ENV === 'production'
-const isDevelopment = NODE_ENV === 'development'
 
 module.exports = {
   mode: isProduction ? 'production' : 'development',
   bail: isProduction,
-  devtool: isDevelopment ? 'cheap-module-source-map' : 'source-map',
+  devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
   entry: {
     main: path.resolve(__dirname, 'index.js'),
   },
@@ -27,19 +25,7 @@ module.exports = {
   },
   optimization: {
     minimize: isProduction,
-    minimizer: [
-      new OptimizeCSSAssetsPlugin({
-        parser: safePostCssParser,
-        map: {
-          // `inline: false` forces the sourcemap to be output into a
-          // separate file
-          inline: false,
-          // `annotation: true` appends the sourceMappingURL to the end of
-          // the css file, helping the browser find the sourcemap
-          annotation: true,
-        },
-      }),
-    ],
+    minimizer: [new CssMinimizerPlugin()],
   },
   module: {
     rules: [
@@ -47,11 +33,9 @@ module.exports = {
         oneOf: [
           {
             test: /\.css$/,
-            exclude: /\.module\.css$/,
             loader: [
-              isProduction && MiniCssExtractPlugin.loader,
+              isProduction ? MiniCssExtractPlugin.loader : require.resolve('style-loader'),
               require.resolve('thread-loader'),
-              isDevelopment && require.resolve('style-loader'),
               {
                 loader: require.resolve('css-loader'),
                 options: {
@@ -62,49 +46,16 @@ module.exports = {
                 loader: require.resolve('postcss-loader'),
                 options: {
                   postcssOptions: {
-                    config: path.resolve(__dirname, '../postcss.config.js'),
-                    ident: 'postcss',
+                    config: path.resolve(__dirname, '../'),
                   },
                 },
               },
-            ].filter(Boolean),
+            ],
             sideEffects: true,
-          },
-          {
-            test: /\.module\.css$/,
-            loader: [
-              isProduction && MiniCssExtractPlugin.loader,
-              require.resolve('thread-loader'),
-              isDevelopment && require.resolve('style-loader'),
-              {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 1,
-                  modules: {
-                    localIdentName: isProduction ? '[hash:8]' : '[name]___[local]',
-                  },
-                },
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  postcssOptions: {
-                    config: path.resolve(__dirname, '../postcss.config.js'),
-                    ident: 'postcss',
-                  },
-                },
-              },
-            ].filter(Boolean),
           },
         ],
       },
     ],
   },
-  plugins: [
-    isProduction &&
-      new MiniCssExtractPlugin({
-        filename: 'static/css/[name].[contenthash].css',
-        chunkFilename: 'static/css/[name].[contenthash].chunk.css',
-      }),
-  ].filter(Boolean),
+  plugins: [isProduction && new MiniCssExtractPlugin()].filter(Boolean),
 }
